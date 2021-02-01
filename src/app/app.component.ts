255ad4c020a2be8a52023c33b181aba5ee524a5a
Molchanova Tatyana <mtuspeh@gmail.com>
2021-02-01 08:12:10 +0200

    http api angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NewServiceService } from "./new-service.service";
+import { HttpClient } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'http';
+
+  response: any;
+  id: number;
+
+    constructor(private service: NewServiceService,
+                private http: HttpClient) {}
+
+    search() {
+      this.http.get('https://jsonplaceholder.typicode.com/users/' + this.id)
+          .subscribe((response) => {
+            this.response = response;
+            console.log('this.response', this.response);
+          })
+    }
+
 }
